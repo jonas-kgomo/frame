3bd45d2304a82369266831e58121a331ab6d329f
jonas-kgomo <jonaskgmoo@gmail.com>
2024-09-06 16:16:39 +0200

    blog

--- app/components/ui/radial.tsx
+++ app/components/ui/radial.tsx
@@ -79,7 +79,7 @@ const renderActiveShape = (props) => {
         y={ey}
         textAnchor={textAnchor}
         fill="#333"
-      >{`PV ${value}`}</text>
+      >{`Viewpoint ${value}`}</text>
       <text
         x={ex + (cos >= 0 ? 1 : -1) * 12}
         y={ey}
